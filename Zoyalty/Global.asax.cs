60e91089f7c4943f8f421faf550a1085e299c70a
santo <santhu.gitam@gmail.com>
2015-09-19 06:00:18 -0700

    1st

--- Zoyalty/Global.asax.cs
+++ Zoyalty/Global.asax.cs
@@ -10,7 +10,7 @@ namespace Zoyalty
     public class Global : System.Web.HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
-        {
+        {s
         }
     }
 }
